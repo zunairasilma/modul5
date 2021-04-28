adbdf8a23eda6ad6fc7189f94e3fd58cad54e5a4
zunairasilma <zunaira_silma_28rpl@student.smktelkom-mlg.sch.id>
2021-04-29 04:26:46 +0700

    Modul 5

---
+++ src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        <p>This is Contact Page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Contact'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Homepage</h1>
+        <p>This is Homepage</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Home'
+}
+</script>
